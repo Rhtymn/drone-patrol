7b21885144f6899600b839c94837108959afa4bd
Roihan <hroihan6@gmail.com>
2024-07-10 14:19:52 +0700

    add new getter and setter including unit test

--- entity/drone.go
+++ entity/drone.go
@@ -4,6 +4,7 @@ type Drone struct {
 	travelDistance int
 	posX           int
 	posY           int
+	height         int
 }
 
 func NewDrone(posX, posY int) Drone {
@@ -17,12 +18,32 @@ func (d *Drone) Reset() {
 	d.travelDistance = 0
 }
 
+func (d *Drone) SetPosX(x int) {
+	d.posX = x
+}
+
+func (d *Drone) SetPosY(y int) {
+	d.posY = y
+}
+
+func (d *Drone) SetTravelDistance(dist int) {
+	d.travelDistance = dist
+}
+
+func (d *Drone) SetHeight(h int) {
+	d.height = h
+}
+
 func (d Drone) PosX() int {
 	return d.posX
 }
 
 func (d Drone) PosY() int {
 	return d.posY
+}
+
+func (d Drone) Height() int {
+	return d.height
 }
 
 func (d Drone) TravelDistance() int {
--- entity/drone_test.go
+++ entity/drone_test.go
@@ -19,18 +19,114 @@ func TestNewDrone(t *testing.T) {
 	})
 }
 
+func TestSetPosX(t *testing.T) {
+	t.Run("should set drone position x to the correct position", func(t *testing.T) {
+		posX, posY := 1, 1
+		newPosX := 2
+		drone := entity.NewDrone(posX, posY)
+
+		drone.SetPosX(newPosX)
+
+		if drone.PosX() != newPosX {
+			t.Fatalf("incorrect drone position x")
+		}
+	})
+}
+
+func TestSetPosY(t *testing.T) {
+	t.Run("should set drone position y to the correct position", func(t *testing.T) {
+		posX, posY := 1, 1
+		newPosY := 2
+		drone := entity.NewDrone(posX, posY)
+
+		drone.SetPosY(newPosY)
+
+		if drone.PosY() != newPosY {
+			t.Fatalf("incorrect drone position y")
+		}
+	})
+}
+
+func TestSetTravelDistance(t *testing.T) {
+	t.Run("should set drone travel distance to the correct value given", func(t *testing.T) {
+		posX, posY := 1, 1
+		travelDistance := 3
+		drone := entity.NewDrone(posX, posY)
+
+		drone.SetTravelDistance(travelDistance)
+
+		if drone.TravelDistance() != travelDistance {
+			t.Fatalf("incorrect drone travel distance")
+		}
+	})
+}
+
+func TestSetHeight(t *testing.T) {
+	t.Run("should set drone height to the correct value given", func(t *testing.T) {
+		posX, posY := 1, 1
+		height := 3
+		drone := entity.NewDrone(posX, posY)
+
+		drone.SetHeight(height)
+
+		if drone.Height() != height {
+			t.Fatalf("incorrect drone height")
+		}
+	})
+}
+
 func TestDronePosX(t *testing.T) {
 	t.Run("should return correct drone position x", func(t *testing.T) {
+		posX, posY := 1, 1
+		drone := entity.NewDrone(posX, posY)
+		drone.SetPosX(2)
 
+		res := drone.PosX()
+
+		if res != 2 {
+			t.Fatalf("incorrect drone position x")
+		}
 	})
 }
 
 func TestDronePosY(t *testing.T) {
 	t.Run("should return correct drone position y", func(t *testing.T) {
+		posX, posY := 1, 1
+		drone := entity.NewDrone(posX, posY)
+		drone.SetPosY(3)
 
+		res := drone.PosY()
+
+		if res != 3 {
+			t.Fatalf("incorrect drone position x")
+		}
 	})
 }
 
 func TestDroneTravelDistance(t *testing.T) {
-	t.Run("should return correct drone travel distance", func(t *testing.T) {})
+	t.Run("should return correct drone travel distance", func(t *testing.T) {
+		posX, posY := 1, 1
+		drone := entity.NewDrone(posX, posY)
+		drone.SetTravelDistance(4)
+
+		res := drone.TravelDistance()
+
+		if res != 4 {
+			t.Fatalf("incorrect drone travel distance")
+		}
+	})
+}
+
+func TestDroneHeight(t *testing.T) {
+	t.Run("should return correct drone height", func(t *testing.T) {
+		posX, posY := 1, 1
+		drone := entity.NewDrone(posX, posY)
+		drone.SetHeight(5)
+
+		res := drone.Height()
+
+		if res != 5 {
+			t.Fatalf("incorrect drone height")
+		}
+	})
 }
