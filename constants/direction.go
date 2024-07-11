f668ce89e1a9ee61cae772b1e1393b83835efb60
Roihan <hroihan6@gmail.com>
2024-07-11 16:56:44 +0700

    add comment on direction constants

--- constants/direction.go
+++ constants/direction.go
@@ -1,7 +1,9 @@
 package constants
 
+// Direction is predeclared identifier representing basic compass points type
 type Direction uint
 
+// It's constants for representing compass point
 const (
 	North = iota + 1
 	East
