345f61995b9d2abf19cecd862b65a011173d7170
Roihan <hroihan6@gmail.com>
2024-07-11 16:53:55 +0700

    add comment on domain interface

--- domain/command.go
+++ domain/command.go
@@ -1,5 +1,8 @@
 package domain
 
+// Commander is interface for creating an drone commands
+// Action method expected for implementing certain drone action
+// such as move to west, north, south, and many more
 type Commander interface {
 	Action(d Droner, dist, meterPerDist int)
 }
--- domain/drone.go
+++ domain/drone.go
@@ -2,6 +2,9 @@ package domain
 
 import "github.com/SawitProRecruitment/JuniorBackendEngineering/constants"
 
+// Droner is interface for creating drone object/struct
+// It's contains basic getter and setter which expected
+// each drone have this functionality
 type Droner interface {
 	Height() int
 	PosX() int
