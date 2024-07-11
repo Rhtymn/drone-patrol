a3ea30a26fa6148a7149c954bf8789b0289601f5
Roihan <hroihan6@gmail.com>
2024-07-11 16:49:49 +0700

    add comment on util function

--- util/transformer.go
+++ util/transformer.go
@@ -7,11 +7,16 @@ import (
 	"github.com/SawitProRecruitment/JuniorBackendEngineering/apperror"
 )
 
+// IntSlice transform separated white space numeric string to slice of int
+// If given string with separator other than white space, it will return an internal error
+// If given non numeric string, it will also return an internal error
 func IntSlice(val string) ([]int, error) {
+	// create array of string from given string (separated by white space)
 	arrString := strings.Fields(val)
 	intSlice := []int{}
 
 	for i := 0; i < len(arrString); i++ {
+		// convert string to int
 		i, err := strconv.Atoi(arrString[i])
 		if err != nil {
 			return nil, apperror.Wrap(err)
--- util/validator.go
+++ util/validator.go
@@ -1,5 +1,8 @@
 package util
 
+// Range check given val is in range from lower to higher (inclusive)
+// It will return false if given val doesn't not in range from lower to higher
+// It will return true if given val which in range from to lower to higher
 func Range(val, lower, higher int) bool {
 	if val < lower || val > higher {
 		return false
